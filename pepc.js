ef9a3ae3fafafbc3dd730a020d7b420f27ede962
Ragini Soni <115521849+Nayabgirl@users.noreply.github.com>
2022-12-10 03:14:35 +0530

    Its a new landing page

---
+++ pepc.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.pepsi').src = anything; 
+}
+function changeBgColor(Color){
+    const sec = document.querySelector(".sec");
+    sec.style.background = Color;
+}
